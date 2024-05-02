8377ecc2d14fbaf84d440f202f774d733fefd637
villainscode <villainscode@gmail.com>
2024-05-02 10:47:21 +0900

    init

---
+++ src/main/java/net/mingleup/guide/MainApp.java
@@ -0,0 +1,12 @@
+package net.mingleup.guide;
+
+/**
+ * @author CodeVillains
+ * @description :
+ */
+public class MainApp {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
